2c6f183dfa73a41dc9629e50e9c92db06466711a
Angel <o.gotalsky@gmail.com>
2013-12-03 14:38:42 +0000

    add issue bean

---
+++ src/main/java/org/training/issuetracker/beans/Issue.java
@@ -0,0 +1,11 @@
+package org.training.issuetracker.beans;
+
+public class Issue {
+	private String summary;
+	private String description;
+	private String status;
+	private String type;
+	private String priority;
+	private String build;
+	private User Assinge;
+}
